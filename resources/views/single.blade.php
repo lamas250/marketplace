ab00f3ee531cde049b694f55b357b8110d015065
Igor Lamas <igorlamas1994@gmail.com>
2020-10-20 08:28:54 -0300

    cart controller with session

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -7,12 +7,27 @@
         <img src="{{url('img/no-photo.jpg')}}" class="card-img-top">
     </div>
     <div class="col-6">
-        <h2>{{$product->name}}</h2>
-        <p>{{$product->description}}</p>
-        <h3>R$ {{number_format($product->price,'2',',','.')}}</h3>
-        <span>
-            Loja: {{$product->store->name}}
-        </span>
+        <div>
+            <h2>{{$product->name}}</h2>
+            <p>{{$product->description}}</p>
+            <h3>R$ {{number_format($product->price,'2',',','.')}}</h3>
+            <span>
+                Loja: {{$product->store->name}}
+            </span>
+        </div>
+        <div class="product-add">
+            <form action="{{route('cart.add')}}" method="post">
+                @csrf
+                <div class="form-group">
+                    <input type="hidden" name="product[name]" value="{{$product->name}}">
+                    <input type="hidden" name="product[price]" value="{{$product->price}}">
+                    <input type="hidden" name="product[slug]" value="{{$product->slug}}">
+                    <label>Quantidade</label>
+                    <input type="number" name="product[amount]" class="form-control col-md-2" value="1">
+                </div>
+                <button href="" class="btn btn-lg btn-danger">Comprar</button>
+            </form>
+        </div>
     </div>
 </div>
 <div class="row">
